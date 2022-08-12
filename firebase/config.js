94acdaefd42c03937929c0382fc135f04badb96c
Jay <julesmontojo1@gmail.com>
2022-08-13 04:35:06 +0900

    fix: deployment env variables

--- firebase/config.js
+++ firebase/config.js
@@ -3,13 +3,13 @@ const {getAuth} = require("firebase/auth");
 require("dotenv").config();
 
 const firebaseConfig = {
-  apiKey: process.env.FB_API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MSG_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  apiKey: process.env.FB_API_KEY || process.env.FB_API_KEY_LOCAL,
+  authDomain: process.env.AUTH_DOMAIN || process.env.AUTH_DOMAIN_LOCAL,
+  projectId: process.env.PROJECT_ID || process.env.PROJECT_ID_LOCAL,
+  storageBucket: process.env.STORAGE_BUCKET || process.env.STORAGE_BUCKET_LOCAL,
+  messagingSenderId: process.env.MSG_SENDER_ID || process.env.MSG_SENDER_ID_LOCAL,
+  appId: process.env.APP_ID || process.env.APP_ID_LOCAL,
+  measurementId: process.env.MEASUREMENT_ID || process.env.MEASUREMENT_ID_LOCAL
 };
 
 // Initialize Firebase
