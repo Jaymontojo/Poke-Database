6159f1457ad9e0404b633f150f63792cda6c2675
Jay <julesmontojo1@gmail.com>
2022-08-13 10:50:58 +0900

    feat: add frontend ui registration

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Auth = require('../models/Auth');
 
 router.post('/register', async(req, res) => {
+  console.log('message received!')
   const {email, password} = req.body
   const newUser = await Auth.register(email, password);
   res.json({"payload": newUser}).status(200);
--- server/models/Auth.js
+++ server/models/Auth.js
@@ -1,10 +1,11 @@
-const {createUserWithEmailAndPassword} = require('firebase/auth');
+const {createUserWithEmailAndPassword, onAuthStateChanged} = require('firebase/auth');
 const { auth } = require('../../firebase/config');
 
 class Auth {
   async register(email, password) {
     try {
       return await createUserWithEmailAndPassword(auth, email, password)
+
     } catch(err){
       return err;
     }
